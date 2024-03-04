a956474641ee75afeb758e1637179a91ab48aea7
Aleksander Deda <adeda@microfin.al>
2024-03-04 16:40:08 +0100

    first commit

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,48 @@
+import { useState, useCallback} from 'react';
+import quizCompleteImg from '../assets/quiz-complete.png';
+import QUESTIONS from '../questions.js';
+import QuestionTimer from './QuestionTimer.jsx';
+
+
+export default function Quiz(){
+    const [userAnswers, setUserAnswers] = useState([])
+    // computing the index of active question index
+    const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+    
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer){
+        setUserAnswers((prevUserAnswers) => {
+            return [...prevUserAnswers, selectedAnswer]
+        })
+    }, [])
+    
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
+    if (quizIsComplete) {
+        return (
+            <div id="summary">
+                <img src={quizCompleteImg} alt="Trophy icon" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        )
+    }
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+    return (
+        <div id="quiz">
+            <div id="question">
+                <QuestionTimer 
+                    key={activeQuestionIndex} // the key here re-mounts the QuestionTimer component for every quizz question
+                    timeout={10000} 
+                    onTimeout={handleSkipAnswer} 
+                />
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id="answers">
+                    {shuffledAnswers.map(answer => 
+                    <li key={answer} className="answer">
+                        <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                    </li>)}
+                </ul>
+            </div>
+        </div>
+    )
+}
